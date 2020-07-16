bd1594d3868272d7c63ec13e2fb2a9e1b9826406
Change ZH <46740575+ChangeZH@users.noreply.github.com>
2020-07-16 19:34:01 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,54 +1,48 @@
 import os
-import cv2
-import json
-import torch
-import numpy as np
-from tqdm import tqdm
+from loss import *
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
-class Load_Dataset(Dataset):
-	"""docstring for Load_Dataset"""
-	def __init__(self,root_dir,transforms):
-		super(Load_Dataset, self).__init__()
-		self.root_dir=root_dir
-		self.transforms=transforms
-		self.imageidlist=self.load_list()
+class FusionDataset(Dataset):
+    """docstring for FusionDataset"""
 
-	def __len__(self):
-		return len(self.imageidlist)
+    def __init__(self, root_path, transform=None):
+        super(FusionDataset, self).__init__()
+        self.root_path = root_path
+        if transform is not None:
+            self.transform = transform
+        else:
+            self.transform = None
+        self.dir_list = os.listdir(self.root_path)
 
-	def __getitem__(self,id):
-		vis,ir=self.load_image(id)
-		W,H=vis.size
-		vis=self.transforms(vis)
-		ir=self.transforms(ir)
-		return [vis,ir]
+    def __len__(self):
+        return len(self.dir_list)
 
-	def load_list(self):
-		return [[os.path.join(self.root_dir,x,os.listdir(os.path.join(self.root_dir,x))[0]),\
-				os.path.join(self.root_dir,x,os.listdir(os.path.join(self.root_dir,x))[1])] \
-				for x in os.listdir(self.root_dir)]
+    def __getitem__(self, id):
+        return self.load_img(id)
 
-	def load_image(self,image_id):
-		image_path=self.imageidlist[image_id]
-		vis=Image.open(image_path[0])
-		ir=Image.open(image_path[1])
-		vis=vis.convert('L')
-		ir=ir.convert('L')
-		# if len(image.split())==1:
-		# 	image=Image.merge('RGB',(image,image,image))
-		return vis,ir
+    def load_img(self, id):
+        img_path = os.path.join(self.root_path, self.dir_list[id])
+        img_list = os.listdir(img_path)
+        img_path_1 = os.path.join(img_path, img_list[0])
+        img_path_2 = os.path.join(img_path, img_list[1])
+        img_1 = Image.open(img_path_1)
+        img_2 = Image.open(img_path_2)
+        img_1=img_1.convert('L')
+        img_2 = img_2.convert('L')
+        if self.transform is not None:
+            img_1 = self.transform(img_1)
+            img_2 = self.transform(img_2)
+        return {'img_1': img_1, 'img_2': img_2}
 
-if __name__=='__main__':
-	root_dir='./datasets/TNO'
-	val_dataset=Load_Dataset(root_dir,
-							transforms.Compose([transforms.Resize((512,512)),
-												transforms.ToTensor()]))
-	val_generator=DataLoader(val_dataset,batch_size=2,shuffle=True)
-	num_iter_per_epoch=len(val_generator)
-	print(num_iter_per_epoch)
 
-	for iter,data in enumerate(val_generator):
-		print('iter',iter,data[0].shape,data[1].shape)
+if __name__ == '__main__':
+    train_dataset = FusionDataset('./data/TNO/', transforms.Compose([transforms.Resize((512, 512)),
+                                                                     transforms.ToTensor()]))
+    train_generator = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    for x, y in enumerate(train_generator):
+        print(x, y['img_1'].shape, y['img_2'].shape)
+# img1,img2,img=load_train_data('./data/TNO/',1,1)
+# print(img1.shape,img2.shape)
